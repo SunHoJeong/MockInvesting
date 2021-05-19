266d95d6d5f36321a90e7a22243293bd92a9ee46
Hoon <hogog8@naver.com>
2021-05-19 16:16:57 +0900

    sh send msg practice commit

--- app/src/main/java/com/sec/mockinvesting/MainActivity.kt
+++ app/src/main/java/com/sec/mockinvesting/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.sec.mockinvesting
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+
+const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("Hello world!")
-        println("sh hello world")
+    }
+
+    fun sendMessage(view: View){
+        val editText = findViewById<EditText>(R.id.editText)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
     }
 }
