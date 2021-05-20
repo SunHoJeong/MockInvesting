f55e10cb628b0fcf3cbb458de83162fb10fa3831
2YoungJae <dldudwo10@naver.com>
2021-05-20 23:15:03 +0900

    -Change IndexName and Term to use annotation. -Fix market_detail to use layout as root layout

--- app/src/main/java/com/sec/mockinvesting/stock/message/Term.kt
+++ app/src/main/java/com/sec/mockinvesting/stock/message/Term.kt
@@ -1,5 +1,15 @@
 package com.sec.mockinvesting.stock.message
 
-enum class Term {
-    DAY, WEEK, MONTH, YEAR, FIVEYEAR, MAXIMUM
-}
+import androidx.annotation.StringDef
+
+
+@Retention(AnnotationRetention.SOURCE)
+@StringDef(DAY, WEEK, MONTH, YEAR, FIVE_YEAR, MAXIMUM)
+annotation class Term
+
+const val DAY = "DAY"
+const val WEEK = "WEEK"
+const val MONTH = "MONTH"
+const val YEAR = "YEAR"
+const val FIVE_YEAR = "FIVE_YEAR"
+const val MAXIMUM = "MAXIMUM"
