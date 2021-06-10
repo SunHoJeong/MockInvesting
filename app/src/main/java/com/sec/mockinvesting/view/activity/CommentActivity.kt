26b614320360e07463e3f760e261cd7513c675e7
david <mintsandwitch@gmail.com>
2021-06-10 21:54:32 +0900

    temporary commit - comment

--- app/src/main/java/com/sec/mockinvesting/view/activity/CommentActivity.kt
+++ app/src/main/java/com/sec/mockinvesting/view/activity/CommentActivity.kt
@@ -1,8 +1,6 @@
 package com.sec.mockinvesting.view.activity
 
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.sec.mockinvesting.R
@@ -12,17 +10,12 @@ import com.sec.mockinvesting.view.fragment.CommentFragment
 class CommentActivity : AppCompatActivity() {
     private lateinit var binding: CommentActivityLayoutBinding
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         binding = DataBindingUtil.setContentView(this, R.layout.comment_activity_layout)
 
         supportFragmentManager.beginTransaction()
             .add(R.id.comment_fragment_container, CommentFragment()).commit()
 
-        super.onCreate(savedInstanceState, persistentState)
-    }
-
-    override fun onResume() {
-        Log.i("CommentActivity", "It's not easy")
-        super.onResume()
+        super.onCreate(savedInstanceState)
     }
 }
